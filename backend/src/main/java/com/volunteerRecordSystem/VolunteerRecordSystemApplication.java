92c9d02e99a310112645c8d24473a206a4de5ff9
markyaooo <yaomark0216@gmail.com>
2026-07-09 18:21:05 +0800

    chore: initialize folder structure

---
+++ backend/src/main/java/com/volunteerRecordSystem/VolunteerRecordSystemApplication.java
@@ -0,0 +1,11 @@
+package com.volunteerRecordSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerRecordSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerRecordSystemApplication.class, args);
+    }
+}
